85a69f642b4ab78c38da8d7c81cd6d90566247c8
VasantDave <davevasant1997@gmail.com>
2023-10-07 10:22:19 +0530

    Create categories_data_details.dart

---
+++ lib/file_data/categories_data_details.dart
@@ -0,0 +1,115 @@
+class CategorieDetails {
+  String categorieId;
+  String categorieName;
+  String categorieImagePath;
+
+  CategorieDetails(
+      this.categorieId, this.categorieName, this.categorieImagePath);
+}
+
+List<Map<String, dynamic>> categorieList = [
+  {
+    'Categorie Id': '1',
+    'Categorie Name': 'Fruits and \n Vegetables',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/58a2654720211117-164849.jepg'
+  },
+  {
+    'Categorie Id': '2',
+    'Categorie Name': 'Grains,Oils & \n Masalas',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/ee7701af20211117-163912.jepg',
+  },
+  {
+    'Categorie Id': '3',
+    'Categorie Name': 'Dairy',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/338a9de420211117-164300.jepg',
+  },
+  {
+    'Categorie Id': '4',
+    'Categorie Name': 'Personal Care',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/7028cd8820211117-164151.jepg',
+  },
+  {
+    'Categorie Id': '5',
+    'Categorie Name': 'Household & \n Cleaning',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/2eda9b7820211117-164056.jepg',
+  },
+  {
+    'Categorie Id': '6',
+    'Categorie Name': 'Maternity & \nBaby Care',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/8438b95f20211117-164230.jepg',
+  }
+];
+List<Map<String, dynamic>> categorieListPage = [
+  {
+    'Categorie Id': '1',
+    'Categorie Name': 'Fruits & Vegetables',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/58a2654720211117-164849.jepg'
+  },
+  {
+    'Categorie Id': '2',
+    'Categorie Name': 'Grains,Oils & Masalas',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/ee7701af20211117-163912.jepg',
+  },
+  {
+    'Categorie Id': '3',
+    'Categorie Name': 'Dairy',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/338a9de420211117-164300.jepg',
+  },
+  {
+    'Categorie Id': '4',
+    'Categorie Name': 'Personal Care',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/7028cd8820211117-164151.jepg',
+  },
+  {
+    'Categorie Id': '5',
+    'Categorie Name': 'Household & Cleaning',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/2eda9b7820211117-164056.jepg',
+  },
+  {
+    'Categorie Id': '6',
+    'Categorie Name': 'Maternity & Baby Care',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/8438b95f20211117-164230.jepg',
+  },
+  {
+    'Categorie Id': '7',
+    'Categorie Name': 'Health & Medicine',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/4a92ccee20211117-164242.jepg',
+  },
+  {
+    'Categorie Id': '8',
+    'Categorie Name': 'Chocolates & Snacks',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/f5df256920211117-165312.jepg',
+  },
+  {
+    'Categorie Id': '9',
+    'Categorie Name': 'Dry Fruits & Sweets',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/a0a2313220211117-164033.jepg',
+  },
+  {
+    'Categorie Id': '10',
+    'Categorie Name': 'Ready to Eat',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/bae0a1fd20211117-165330.jepg',
+  },
+  {
+    'Categorie Id': '11',
+    'Categorie Name': 'Stationery',
+    'Categorie Image':
+        'https://retailer-onboarding-kyc.s3.amazonaws.com/9af8a74a20211117-165404.jepg',
+  }
+];
