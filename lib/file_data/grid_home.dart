7c4b39e35e6085e0bd19f6e8b13a106727098aff
VasantDave <davevasant1997@gmail.com>
2023-10-07 10:22:31 +0530

    Create grid_home.dart

---
+++ lib/file_data/grid_home.dart
@@ -0,0 +1,40 @@
+class GridHome {
+  String imagePath;
+
+  GridHome(this.imagePath);
+}
+
+List<GridHome> gridHome = [
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1690483216_Web_Mumbai_PremiumFruits.jpg?im=Resize=(1680,320)'),
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1677043775_Website_Wednesday-Bonus1680x320.jpg?im=Resize=(1680,320)'),
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1690954366_Nutritious_Dals_and_Pulses_Desktop.jpg?im=Resize=(1680,320)'),
+];
+
+List<GridHome> gridHomeBank = [
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1690804972_BOB_CC_1240x209.jpg?im=Resize=(1241,195)'),
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1690804998_Induslnd_Bank_1240x209.jpg?im=Resize=(1241,195)'),
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1690805026_AU_Bank_1240x209.jpg?im=Resize=(1241,195)'),
+  GridHome(
+      'https://www.jiomart.com/images/cms/aw_rbslider/slides/1686819477_ShopForGroceries1240.jpg?im=Resize=(1241,195)'),
+];
+
+List<GridHome> gridHomeOffer = [
+  GridHome(
+      'https://www.bigbasket.com/media/uploads/banner_images/hp_bcd_m_bcd_250723_400.jpg'),
+  GridHome(
+      'https://www.bigbasket.com/media/uploads/banner_images/hp_m_babycare_250723_400.jpg'),
+  GridHome(
+      'https://retailer-onboarding-kyc.s3.amazonaws.com/b993752020230731-073614.jepg'),
+  GridHome(
+      'https://retailer-onboarding-kyc.s3.amazonaws.com/279faa2420230731-073722.jepg'),
+  GridHome(
+      'https://retailer-onboarding-kyc.s3.amazonaws.com/3c5ac25620230731-073952.jepg'),
+  GridHome(
+      'https://retailer-onboarding-kyc.s3.amazonaws.com/fa755d5420230731-073857.jepg'),
+];
