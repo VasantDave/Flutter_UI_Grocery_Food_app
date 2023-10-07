f4715f2c6f5ce6a2687ae668caea3a2fd79e2754
VasantDave <davevasant1997@gmail.com>
2023-10-07 10:22:36 +0530

    Create intro_details.dart

---
+++ lib/file_data/intro_details.dart
@@ -0,0 +1,22 @@
+class IntroDetails {
+  String? imagePath;
+  String? title;
+  String? subTitle;
+
+  IntroDetails(this.imagePath, this.title, this.subTitle);
+}
+
+List<IntroDetails> introDetails = [
+  IntroDetails(
+      'https://lottie.host/5cf60ddc-1f5a-46fe-be76-a7705fb138f9/YoBfKRP9oX.json',
+      'Wide Assortment',
+      "40,000+ Products from 12 diverse\ncategories to shop from"),
+  IntroDetails(
+      'https://lottie.host/2700981a-31c5-4f22-89f6-5ac49d0d0656/xvQkJKjnJH.json',
+      'Har Din Sasta!',
+      "Great deals & offers everyday"),
+  IntroDetails(
+      'https://lottie.host/467d9de7-0800-4b33-89e2-8807d6c7bfe4/YhhtwXEkHA.json',
+      'Incredible Delivery Speed',
+      "Bringing the store to your door"),
+];
