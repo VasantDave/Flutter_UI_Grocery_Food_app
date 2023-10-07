edbabce20c2d233007cf2c34f0b6e8af5a49b2f0
VasantDave <davevasant1997@gmail.com>
2023-10-07 10:22:43 +0530

    Create sales_items_data.dart

---
+++ lib/file_data/sales_items_data.dart
@@ -0,0 +1,130 @@
+class SalesItemsData {
+  String itemName;
+  int itemRate;
+  String itemImagePath;
+  String itemActualRate;
+  String itemOffer;
+
+  SalesItemsData(this.itemName, this.itemRate, this.itemImagePath,
+      this.itemActualRate, this.itemOffer);
+}
+
+List<Map<String, dynamic>> salesItemsTopDeals = [
+  {
+    'Item Name': 'Pyramid Traders 1KG Kashmiri \nMamra Almonds (Badam Giri)',
+    'Item Rate': '1199',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/rvrgcvfzmb/pyramid-traders-1kg-kashmiri-mamra-almonds-badam-giri-product-images-orvrgcvfzmb-p598925282-0-202307181608.png?im=Resize=(420,420)',
+    'Item Actual Rate': '4000',
+    'Item Offer': '70% Off',
+  },
+  {
+    'Item Name': 'Clinic Plus+ Strong & \nLong Health Shampoo 1 L',
+    'Item Rate': '405',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/491934236/clinic-plus-strong-long-health-shampoo-1-l-product-images-o491934236-p590317996-0-202203151906.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '810',
+    'Item Offer': '50% Off',
+  },
+  {
+    'Item Name': 'Potato Low Sugar 2 kg',
+    'Item Rate': '70',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/590001788/potato-low-sugar-2-kg-product-images-o590001788-p590001788-0-202207272007.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '74',
+    'Item Offer': '2% Off',
+  },
+  {
+    'Item Name': 'Gulab Sungold Refined \nSunflower Oil 15 L (Tin)',
+    'Item Rate': '1649',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/490794273/gulab-sungold-refined-sunflower-oil-15-l-tin-product-images-o490794273-p490794273-0-202209100208.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '3563',
+    'Item Offer': '53% Off',
+  },
+  {
+    'Item Name':
+        'Maggi 2-Minutes Masala Instant \nNoodles (No Onion No Garlic) 70 g',
+    'Item Rate': '18',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/491282527/maggi-2-minutes-masala-instant-noodles-no-onion-no-garlic-70-g-product-images-o491282527-p491282527-0-202203150435.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '20',
+    'Item Offer': '10% Off',
+  },
+  {
+    'Item Name': 'Amul Pure Ghee 1 L (Tin)',
+    'Item Rate': '631',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/490001521/amul-pure-ghee-1-l-tin-product-images-o490001521-p490001521-0-202206061807.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '650',
+    'Item Offer': '2% Off',
+  },
+  {
+    'Item Name': 'BB Royal Cumin/Jeera/Jeerige\n - Whole, 100 g Pouch',
+    'Item Rate': '82',
+    'Item Image Path':
+        'https://www.bigbasket.com/media/uploads/p/l/10000479_13-bb-royal-cuminjeera-whole.jpg',
+    'Item Actual Rate': '120',
+    'Item Offer': '32% Off',
+  },
+];
+List<Map<String, dynamic>> salesItemsTopPicks = [
+  {
+    'Item Name':
+        'Daawat Rozana Gold Basmati \nRice 5 kg+ Saffola Gold Rice \nBran Based Blended Oil 3 L',
+    'Item Rate': '749',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/600520445/daawat-rozana-gold-basmati-rice-5-kg-saffola-gold-rice-bran-based-blended-oil-3-l-product-images-o600520445-p590032549-0-202307072039.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '1250',
+    'Item Offer': '40% Off',
+  },
+  {
+    'Item Name': 'MyFitness Chocolate Crunchy\n Peanut Butter 510 gms',
+    'Item Rate': '274',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/rvkbcgyvwk/myfitness-chocolate-crunchy-peanut-butter-510-gms-product-images-orvkbcgyvwk-p595324637-0-202211151319.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '320',
+    'Item Offer': '14% Off',
+  },
+  {
+    'Item Name': 'Potato Low Sugar 2 kg',
+    'Item Rate': '70',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/590001788/potato-low-sugar-2-kg-product-images-o590001788-p590001788-0-202207272007.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '74',
+    'Item Offer': '2% Off',
+  },
+  {
+    'Item Name': 'Gulab Sungold Refined \nSunflower Oil 15 L (Tin)',
+    'Item Rate': '1649',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/490794273/gulab-sungold-refined-sunflower-oil-15-l-tin-product-images-o490794273-p490794273-0-202209100208.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '3563',
+    'Item Offer': '53% Off',
+  },
+  {
+    'Item Name':
+        'Maggi 2-Minutes Masala Instant \nNoodles (No Onion No Garlic) 70 g',
+    'Item Rate': '18',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/491282527/maggi-2-minutes-masala-instant-noodles-no-onion-no-garlic-70-g-product-images-o491282527-p491282527-0-202203150435.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '20',
+    'Item Offer': '10% Off',
+  },
+  {
+    'Item Name': 'Amul Pure Ghee 1 L (Tin)',
+    'Item Rate': '631',
+    'Item Image Path':
+        'https://www.jiomart.com/images/product/original/490001521/amul-pure-ghee-1-l-tin-product-images-o490001521-p490001521-0-202206061807.jpg?im=Resize=(420,420)',
+    'Item Actual Rate': '650',
+    'Item Offer': '2% Off',
+  },
+  {
+    'Item Name': 'BB Royal Cumin/Jeera/Jeerige\n - Whole, 100 g Pouch',
+    'Item Rate': '82',
+    'Item Image Path':
+        'https://www.bigbasket.com/media/uploads/p/l/10000479_13-bb-royal-cuminjeera-whole.jpg',
+    'Item Actual Rate': '120',
+    'Item Offer': '32% Off',
+  },
+];
